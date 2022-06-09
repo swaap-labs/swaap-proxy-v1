fddd1fa67a5645d914d025086f1b0ecdf3ed832d
Christian Dahdah <christiandahdah@gmail.com>
2022-06-09 17:50:05 +0200

    fix: removed unnecessary Num contract on deployment

--- migrations/1_Factory.js
+++ migrations/1_Factory.js
@@ -1,4 +1,3 @@
-const Num = artifacts.require("Num");
 const Math = artifacts.require("Math");
 const GeometricBrownianMotionOracle = artifacts.require("GeometricBrownianMotionOracle");
 const Factory = artifacts.require("Factory");
@@ -7,14 +6,10 @@ module.exports = async function (deployer,network, accounts) {
 
 	// Factory only deployed from test
 	if(network === undefined || network == "test" || network === "dev-fork" || network === "dev") {
-		deployer.deploy(Num);
-		deployer.link(Num, GeometricBrownianMotionOracle);
 		deployer.deploy(GeometricBrownianMotionOracle);
-		deployer.link(Num, Math);
 		deployer.link(GeometricBrownianMotionOracle, Math);
 		deployer.deploy(Math);
 		deployer.link(Math, Factory);
-		deployer.link(Num, Factory);
 		deployer.deploy(Factory);
 	}
 	
